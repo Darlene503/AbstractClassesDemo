e2195127fc3a03c7fe4f4cd8241860d7da37b1e6
Darlene503 <duyn503@gmail.com>
2023-08-04 21:17:59 -0700

    finding one error

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.VisualBasic;
 
 namespace ConsoleUI
 {
@@ -10,44 +12,83 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-            /*
-             * Todo follow all comments!! 
-             */
+
 
             #region Vehicles
 
-            /*
-             * Create an abstract class called Vehicle
-             * The vehicle class shall have three string properties Year, Make, and Model
-             * Set the defaults to something generic in the Vehicle class
-             * Vehicle shall have an abstract method called DriveAbstract with no implementation
-             * Vehicle shall have a virtual method called DriveVirtual with a base implementation.
-             */
+            var vehicles = new List<Vehicle>();
 
-            /* 
-             * Now create 2 non-abstract classes: Car and Motorcycle, that inherit from Vehicle
-             * Add a distict property in the 2 derived classes such as HasTrunk for Car and HasSideCart for Motorcycle
-             * Provide the implementations for the abstract methods
-             * Only in the Motorcycle class will you override the virtual drive method
-            */
 
-            // Create a list of Vehicle called vehicles
+            var car1 = new Car
+            {
+                HasTrunk = true,
+                Year = "2021",
+                Make = "Toyota",
+                Model = "Camry",
+            };
+            vehicles.Add(car1);
 
-            /*
-             * Create 4 instances: 1 Car, 1 Motorcycle, and then 2 instances of type Vehicle (use explicit typing) but use constuctors from derived classes
-             * 
-             * Set the properties with object initializer syntax
-             */
+            var motorcycle1 = new Motorcycle
+            {
+                HasSideCart = true,
+                Year = "2021",
+                Make = "Kawasaki",
+                Model = "Ninja",
+            };
+            vehicles.Add(motorcycle1);
 
-            /*
-             * Add the 4 vehicles to the list
-             * Using a foreach loop iterate over each of the properties
-             */
+            Vehicle vehicle1 = new Car
+            {
+                Make = "Ford",
+                Year = "1955",
+                Model = "Mustang"
+            };
+            vehicles.Add(vehicle1);
+            {
 
-            // Call each of the drive methods for one car and one motorcycle
+                Vehicle vehicle2 = new Motorcycle
+                {
+                    Make = "Harley Davidson",
+                    Year = "2021",
+                    Model = "Fat Boy",
+                };
+                vehicles.Add(vehicle2);
 
-            #endregion            
-            Console.ReadLine();
+
+
+                foreach (var vehicle in vehicles)
+                {
+                    Console.WriteLine($"Year:{vehicle.Year}");
+                    Console.WriteLine($"Make:{vehicle.Make}");
+                    Console.WriteLine($"Model:{vehicle.Model}");
+                    Console.WriteLine();
+                    Console.WriteLine();
+                }
+
+
+                #endregion
+
+            }
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
